4bc3b45a67e7ac8b44c0886ccc57920d2279e59c
Anla70 <98649933+Anla70@users.noreply.github.com>
2023-12-27 12:04:38 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -19,36 +19,54 @@ const url = "http://localhost:3000/recipe";
 fetch(url)
   .then((response) => response.json())
   .then((recipe) => {
-    const grid = document.createElement("div");
-    grid.className = "grid";
+    const recipeList = document.createElement("ul");
 
-    recipe.forEach((recipe, index) => {
-      // Skapar en div för varje användare som kommer att innehålla både etiketten och användarens detaljer
-      const userDiv = document.createElement("div");
-      userDiv.className = "grid-item";
+    recipe.forEach((recipe) => {
+      const recipeList = document.createElement("li");
+      listItem.style.borderColor = recipe.color;
 
-      // Skapar en etikett för varje användare och nummer
-      const memberLabel = document.createElement("div");
-      memberLabel.className = "member-label";
-      memberLabel.innerHTML = `Member<br>#${index + 1}`; // Sätter Member och #, vi har index som börjar på 0 och ökar med 1 för varje användare
+      listItem.innerHTML = `<p>ID: ${recipe.id} <br> Name: ${recipe.recipeName} ${recipe.recipeIngredients}
+      <br> Description: ${recipe.recipeDescription} <br>Färg: ${recipe.color} </p>`;
+      console.log(recipe);
 
-      // Skapar en div för användarens namn och användarnamn
-      const userInfo = document.createElement("div");
-      userInfo.className = "user-info";
-      // Sätter in användarens namn och användarnamn i userInfo
-      userInfo.innerHTML = `<p>${user.firstName} ${user.lastName}<br>Username: ${user.username}</p>`;
-      userInfo.style.backgroundColor = user.color; // Sätter bakgrundsfärgen på userInfo till användarens färg
-
-      // Skapar en etikett för varje användare
-      userDiv.appendChild(memberLabel);
-      userDiv.appendChild(userInfo);
-
-      // Lägger till userDiv till i griden
-      grid.appendChild(userDiv);
+      recipeList.appendChild(recipeList);
     });
-    // Hämtar container från index.html och lägger till griden där
-    document.querySelector(".container").appendChild(grid);
-  })
-  .catch((error) => {
-    console.error("Error fetching users:", error);
+    document.body.appendChild(recipeList);
   });
+
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((recipe) => {
+//     const grid = document.createElement("div");
+//     grid.className = "grid";
+
+//     recipe.forEach((recipe, index) => {
+//       // Skapar en div för varje användare som kommer att innehålla både etiketten och användarens detaljer
+//       const userDiv = document.createElement("div");
+//       userDiv.className = "grid-item";
+
+//       // Skapar en etikett för varje användare och nummer
+//       const memberLabel = document.createElement("div");
+//       memberLabel.className = "member-label";
+//       memberLabel.innerHTML = `Member<br>#${index + 1}`; // Sätter Member och #, vi har index som börjar på 0 och ökar med 1 för varje användare
+
+//       // Skapar en div för användarens namn och användarnamn
+//       const userInfo = document.createElement("div");
+//       userInfo.className = "user-info";
+//       // Sätter in användarens namn och användarnamn i userInfo
+//       userInfo.innerHTML = `<p>${user.firstName} ${user.lastName}<br>Username: ${user.username}</p>`;
+//       userInfo.style.backgroundColor = user.color; // Sätter bakgrundsfärgen på userInfo till användarens färg
+
+//       // Skapar en etikett för varje användare
+//       userDiv.appendChild(memberLabel);
+//       userDiv.appendChild(userInfo);
+
+//       // Lägger till userDiv till i griden
+//       grid.appendChild(userDiv);
+//     });
+//     // Hämtar container från index.html och lägger till griden där
+//     document.querySelector(".container").appendChild(grid);
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching users:", error);
+//   });
