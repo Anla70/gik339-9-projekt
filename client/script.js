891f85a233a57f870322c4b2e1980b706b42343f
ememmy <120389428+ememmy@users.noreply.github.com>
2023-12-28 10:07:13 +0100

    Update

--- client/script.js
+++ client/script.js
@@ -4,33 +4,43 @@ document.addEventListener("DOMContentLoaded", function () {
   fetchAndDisplayRecipes();
 
   const form = document.getElementById("addRecipeForm");
+
   form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     const newRecipe = {
       recipeName: document.getElementById("recipeName").value,
+
       recipeDescription: document.getElementById("recipeDescription").value,
+
       recipeIngredients: document.getElementById("recipeIngredients").value,
+
       recipeInstructions: document.getElementById("recipeInstructions").value,
     };
 
     fetch(url, {
       method: "POST",
+
       headers: {
         "Content-Type": "application/json",
       },
+
       body: JSON.stringify(newRecipe),
     })
       .then((response) => response.json())
+
       .then((data) => {
         console.log("Success:", data);
+
         fetchAndDisplayRecipes(); // Uppdatera receptlistan
       })
+
       .catch((error) => {
         console.error("Error:", error);
       });
 
     // Rensa formuläret efter inskickning
+
     form.reset();
   });
 });
@@ -38,29 +48,43 @@ document.addEventListener("DOMContentLoaded", function () {
 function fetchAndDisplayRecipes() {
   fetch(url)
     .then((response) => response.json())
+
     .then((recipes) => {
       console.log(recipes);
+
       const recipeList = document.createElement("ul");
+
       recipeList.innerHTML = ""; // Rensa tidigare recept
 
       recipes.forEach((recipe) => {
         const listItem = document.createElement("li");
+
         listItem.style.borderTop = "2rem solid";
+
         listItem.style.borderTopColor = recipe.recipeColor;
 
-        listItem.innerHTML = `<p><b>ID:</b> ${recipe.id}</p>
-        <p><b>Beskrivning:</b> ${recipe.recipeDescription}</p>
-        <p><b>Recept:</b> ${recipe.recipeName}</p>
-        <p><b>Ingredienser:</b> ${recipe.recipeIngredients}</p>
+        listItem.innerHTML = `<p><b>ID:</b> ${recipe.id}</p> 
+
+        <p><b>Beskrivning:</b> ${recipe.recipeDescription}</p> 
+
+        <p><b>Recept:</b> ${recipe.recipeName}</p> 
+
+        <p><b>Ingredienser:</b> ${recipe.recipeIngredients}</p> 
+
         <p><b>Instruktioner:</b> ${recipe.recipeInstructions}</p>`;
+
         console.log(recipe);
+
         recipeList.appendChild(listItem);
       });
 
       const container = document.querySelector(".container");
+
       container.innerHTML = ""; // Rensa befintligt innehåll
+
       container.appendChild(recipeList);
     })
+
     .catch((error) => {
       console.error("Error:", error);
     });
@@ -69,20 +93,31 @@ function fetchAndDisplayRecipes() {
 // const url = "http://localhost:5500/recipe";
 
 // fetch(url)
+
 //   .then((response) => response.json())
+
 //   .then((recipes) => {
+
 //     console.log(recipes);
+
 //     const recipeList = document.createElement("ul");
 
 //     recipes.forEach((recipe) => {
+
 //       const recipeList = document.createElement("li");
+
 //       listItem.style.borderColor = recipe.color;
 
 //       listItem.innerHTML = `<p>ID: ${recipe.id} <br> Name: ${recipe.recipeName} ${recipe.recipeIngredients}
+
 //       <br> Description: ${recipe.recipeDescription} <br>Färg: ${recipe.color} </p>`;
+
 //       console.log(recipe);
 
 //       recipeList.appendChild(listItem);
+
 //     });
+
 //     document.body.appendChild(recipeList);
+
 //   });
--- server/server.js
+++ server/server.js
@@ -1,26 +1,39 @@
 // Importerar express.js och sqlite3-modulen
+
 // samt skapar en ny instans av express.js
+
 const express = require("express");
+
 const server = express();
+
 const port = 3000;
+
 const sqlite = require("sqlite3").verbose();
+
 const db = new sqlite.Database("./gik339.db");
 
 // Konfigurerar servern för att hantera JSON-data samt URL-data
+
 server.use(express.json()).use(express.urlencoded({ extended: false }));
+
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+
   res.header("Access-Control-Allow-Headers", "*");
+
   res.header("Access-Control-Allow-Methods", "*");
+
   next();
 });
 
 // Route för att hämta alla recept från databasen. Resultatet skickas till klienten alt felmeddelande
+
 server.get("/recipe", (req, res) => {
   const query = "SELECT * FROM recipe";
 
   db.all(query, (err, rows) => {
     //db.close(); // stänger av den för att den kan skapa problem för efterkommand eanrop
+
     if (err) {
       res.status(500).send(err);
     } else {
@@ -30,13 +43,16 @@ server.get("/recipe", (req, res) => {
 });
 
 // Route för att lägga till nya recept
+
 server.post("/recipe", (req, res) => {
   const recipe = req.body;
+
   const query =
     "INSERT INTO recipe(recipeName, recipeDescription, recipeIngredients, recipeInstructions, recipeImage) VALUES (?,?,?,?,?)";
 
   db.all(query, Object.values(recipe), (err) => {
     db.close();
+
     if (err) {
       res.status(500).send(err);
     } else {
@@ -46,23 +62,34 @@ server.post("/recipe", (req, res) => {
 });
 
 // Route för att uppdatera befintliga recept
+
 server.put("/recipe", (req, res) => {
   const bodyData = req.body;
+
   const id = bodyData.id;
+
   const recipeData = {
     recipeName: bodyData.recipeName,
+
     recipeDescription: bodyData.recipeDescription,
+
     recipeIngredients: bodyData.recipeIngredients,
+
     recipeInstructions: bodyData.recipeInstructions,
+
     recipeImage: bodyData.recipeImage,
   };
 
   let updateString = "";
+
   const columnsArray = Object.keys(recipeData);
+
   columnsArray.forEach((column, i) => {
     updateString += `${column}="${recipe[column]}"`;
+
     if (i !== columnsArray.length - 1) updateString += ",";
   });
+
   const query = `UPDATE recipe SET ${updateString} WHERE id=${id}`;
 
   db.run(query, (err) => {
@@ -75,8 +102,10 @@ server.put("/recipe", (req, res) => {
 });
 
 // Route för att ta bort befintligt valt recept via id
+
 server.delete("/recipe/:id", (req, res) => {
   const id = req.params.id;
+
   const query = `DELETE FROM recipe WHERE id = ${id}`;
 
   db.run(query, (err) => {
@@ -89,106 +118,179 @@ server.delete("/recipe/:id", (req, res) => {
 });
 
 // Startar servern på valt portnummer (variabeln port) och skriver ut meddelande när servern är igång
+
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 // // Importerar express.js och sqlite3-modulen
+
 // // samt skapar en ny instans av express.js
+
 // const express = require("express");
+
 // const server = express();
+
 // const sqlite3 = require("sqlite3").verbose();
+
 // const port = 5500;
 
 // // konfar serven för att hantera JSON-data samt URL-data
+
 // server.use(express.json()).use(express.urlencoded({ extended: false }));
+
 // server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "*");
-//   res.header("Access-Control-Allow-Methods", "*");
-//   next();
+
+//   res.header("Access-Control-Allow-Origin", "*");
+
+//   res.header("Access-Control-Allow-Headers", "*");
+
+//   res.header("Access-Control-Allow-Methods", "*");
+
+//   next();
+
 // });
 
 // // Startar serven på port 3000, när serven är igång skrivs medelande ut
+
 // server.listen(port, () => {
-//   console.log("Server is running on http://localhost:5500 ");
+
+//   console.log("Server is running on http://localhost:5500 ");
+
 // });
 
 // // Route för att hämta alla recept från databasen. Resultatet skickas till klienten alt felmeddelande
+
 // server.get("/recipe", (req, res) => {
-//   const db = new sqlite3.Database("./gik339.db");
 
-//   const query = "SELECT * FROM recipe";
+//   const db = new sqlite3.Database("./gik339.db");
 
-//   db.all(query, (err, rows) => {
-//     db.close();
-//     if (err) {
-//       res.status(500).send(err);
-//     } else {
-//       res.send(rows);
-//     }
-//   });
+//   const query = "SELECT * FROM recipe";
+
+//   db.all(query, (err, rows) => {
+
+//     db.close();
+
+//     if (err) {
+
+//       res.status(500).send(err);
+
+//     } else {
+
+//       res.send(rows);
+
+//     }
+
+//   });
+
 // });
 
 // // Route för att lägga till nya recept
-// server.post("/recipe", (req, res) => {
-//   const db = new sqlite3.Database("./gik339.db");
-//   const recipe = req.body;
-//   const query =
-//     "INSERT INTO recipe(recipeName, recipeDescription, recipeIngredients, recipeInstructions, color) VALUES (?,?,?,?,?)";
 
-//   db.all(query, Object.values(recipe), (err) => {
-//     db.close();
-//     if (err) {
-//       res.status(500).send(err);
-//     } else {
-//       res.send("Receptet sparades");
-//     }
-//   });
+// server.post("/recipe", (req, res) => {
+
+//   const db = new sqlite3.Database("./gik339.db");
+
+//   const recipe = req.body;
+
+//   const query =
+
+//     "INSERT INTO recipe(recipeName, recipeDescription, recipeIngredients, recipeInstructions, color) VALUES (?,?,?,?,?)";
+
+//   db.all(query, Object.values(recipe), (err) => {
+
+//     db.close();
+
+//     if (err) {
+
+//       res.status(500).send(err);
+
+//     } else {
+
+//       res.send("Receptet sparades");
+
+//     }
+
+//   });
+
 // });
 
 // // Route för att uppdatera befintliga recept
+
 // server.put("/recipe", (req, res) => {
-//   const bodyData = req.body;
 
-//   const id = bodyData.id;
-//   const recipeData = {
-//     recipeName: bodyData.recipeName,
-//     recipeDescription: bodyData.recipeDescription,
-//     recipeIngredients: bodyData.recipeIngredients,
-//     recipeInstructions: bodyData.recipeInstructions,
-//     recipeColor: bodyData.recipeColor,
-//   };
+//   const bodyData = req.body;
 
-//   let updateString = "";
-//   const columnsArray = Object.keys(recipeData);
-//   columnsArray.forEach((column, i) => {
-//     updateString += `${column}="${recipe[column]}"`;
-//     if (i !== columnsArray.length - 1) updateString += ",";
-//   });
-//   const query = `UPDATE recipe SET ${updateString} WHERE id=${id}`;
+//   const id = bodyData.id;
 
-//   db.run(query, (err) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send(err);
-//     } else {
-//       res.send("Receptet uppdaterades");
-//     }
-//   });
+//   const recipeData = {
+
+//     recipeName: bodyData.recipeName,
+
+//     recipeDescription: bodyData.recipeDescription,
+
+//     recipeIngredients: bodyData.recipeIngredients,
+
+//     recipeInstructions: bodyData.recipeInstructions,
+
+//     recipeColor: bodyData.recipeColor,
+
+//   };
+
+//   let updateString = "";
+
+//   const columnsArray = Object.keys(recipeData);
+
+//   columnsArray.forEach((column, i) => {
+
+//     updateString += `${column}="${recipe[column]}"`;
+
+//     if (i !== columnsArray.length - 1) updateString += ",";
+
+//   });
+
+//   const query = `UPDATE recipe SET ${updateString} WHERE id=${id}`;
+
+//   db.run(query, (err) => {
+
+//     if (err) {
+
+//       console.log(err);
+
+//       res.status(500).send(err);
+
+//     } else {
+
+//       res.send("Receptet uppdaterades");
+
+//     }
+
+//   });
+
 // });
 
 // // Route för att ta bort befintligt valt recept via id
-// server.delete("/recipe/:id", (req, res) => {
-//   const id = req.params.id;
-//   const query = `DELETE FROM recipe WHERE id = ${id}`;
 
-//   db.run(query, (err) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send(err);
-//     } else {
-//       res.send("Receptet är borttaget");
-//     }
-//   });
+// server.delete("/recipe/:id", (req, res) => {
+
+//   const id = req.params.id;
+
+//   const query = `DELETE FROM recipe WHERE id = ${id}`;
+
+//   db.run(query, (err) => {
+
+//     if (err) {
+
+//       console.log(err);
+
+//       res.status(500).send(err);
+
+//     } else {
+
+//       res.send("Receptet är borttaget");
+
+//     }
+
+//   });
+
 // });
